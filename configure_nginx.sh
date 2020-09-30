8f341e049ed323408c2e5ae64a00a9828f4d45b3
Kalin Kadiev <kkadiev@asteasolutions.com>
2020-09-30 15:43:52 +0300

    Ugly hack for proxying subdomains to paths.

--- configure_nginx.sh
+++ configure_nginx.sh
@@ -1,11 +1,15 @@
 PORT=${PORT:-443}
+TARGET_HOST=${TARGET_HOST:-proxyapp}
 TARGET_PORT=${TARGET_PORT:-80}
+TARGET_PATH=${TARGET_PATH:-.}
 
 echo "Starting Proxy: $PORT"
 echo "Target Docker Port: $TARGET_PORT"
 
 cat nginx.conf.template | \
     sed "s|{{listenPort}}|$PORT|g" | \
+    sed "s|{{targetHost}}|$TARGET_HOST|g" | \
+    sed "s|{{targetPath}}|$TARGET_PATH|g" | \
     sed "s|{{targetPort}}|$TARGET_PORT|g" > /etc/nginx/nginx.conf
 
 # Use exec so nginx can get signals directly
